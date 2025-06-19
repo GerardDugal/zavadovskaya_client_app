26d2aaee8896398fe331c5e70577355ec1341d9a
GerardDugal <gm9152693790@gmail.com>
2025-06-19 12:33:53 +0300

    second try to fix payment on safari

--- lib/data/repositories/payment_repository_impl.dart
+++ lib/data/repositories/payment_repository_impl.dart
@@ -24,6 +24,7 @@ class PaymentRepositoryImpl implements PaymentRepository {
   @override
   Future<PaymentResponse> payForCourse(PaymentRequest paymentRequest) async {
     final headers = await _getHeaders();
+    headers['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1';
     final response = await http.post(
       Uri.parse('$baseUrl/payment/payment/create/for_course/${paymentRequest.courseID}'),
       headers: headers,
